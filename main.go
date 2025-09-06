cc19cc3c0087a6a06f3b36e3c2f1bb9cc5c31f69
Hayden McAlister <haydenmcalister49@gmail.com>
2025-09-06 21:14:16 +1200

    Add logic to update tickers in real time using goroutines

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"flag"
 	"log/slog"
+	"sync"
+	"time"
 
 	"github.com/hmcalister/genron/config"
 	"github.com/hmcalister/genron/ticker"
+	"github.com/spf13/viper"
 )
 
 func main() {
@@ -23,4 +26,45 @@ func main() {
 	slog.Debug("parsed tickers", "tickers", tickers)
 
 	updatePeriod := time.Duration(viper.GetInt64("UpdatePeriod")) * time.Nanosecond
+	var tickerWaitGroup sync.WaitGroup
+	for _, ticker := range tickers {
+		tickerWaitGroup.Go(func() {
+			// An unfortunate name, time.Ticker is a timing device to count a certain time before updating.
+			// We will refer to this as the timer throughout to avoid confusion with a stock ticker.
+			timer := time.NewTicker(updatePeriod)
+
+			for updateTimerTimestamp := range timer.C {
+				updateStartTime := time.Now()
+				ticker.Update()
+				updateDuration := time.Since(updateStartTime)
+
+				slog.Debug("ticker updated",
+					slog.Group("ticker",
+						"name", ticker.String(),
+						"value", ticker.GetValue(),
+					),
+					slog.Group("timing",
+						"timestamp", updateTimerTimestamp,
+						"updateDuration", updateDuration,
+						"expectedUpdatePeriod", updatePeriod,
+					),
+				)
+
+				if updateDuration > updatePeriod {
+					slog.Warn("timer update is lagging behind update period",
+						slog.Group("ticker",
+							"name", ticker.String(),
+							"value", ticker.GetValue(),
+						),
+						slog.Group("timing",
+							"timestamp", updateTimerTimestamp,
+							"updateDuration", updateDuration,
+							"expectedUpdatePeriod", updatePeriod,
+						),
+					)
+				}
+			}
+		})
+	}
+	tickerWaitGroup.Wait()
 }
