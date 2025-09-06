a3a69769835a033dbe89051f0c2dc321a95d445a
Hayden McAlister <haydenmcalister49@gmail.com>
2025-09-06 16:53:00 +1200

    Load config and configure the logger

--- main.go
+++ main.go
@@ -11,4 +11,10 @@ func main() {
 	configFilePath := flag.String("configFilePath", "config.yaml", "Set the file path to the config file. Accepts JSON, YAML, TOML, and envfiles. See README for config specifications.")
 	flag.Parse()
 
+	config.LoadConfig(*configFilePath)
+	logFilePointer := config.ConfigureLogger()
+	if logFilePointer != nil {
+		defer logFilePointer.Close()
+	}
+	slog.Debug("logger configured")
 }
