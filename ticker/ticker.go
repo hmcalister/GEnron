cf834f038414496e1d5b83756b9846345c130e23
Hayden McAlister <haydenmcalister49@gmail.com>
2025-09-06 17:05:45 +1200

    Add start of ticker interface implementations

---
+++ ticker/ticker.go
@@ -0,0 +1,17 @@
+package ticker
+
+// The default ticker interface.
+// All tickers must implement the below methods, but may use vastly different implementations.
+// This allows for, say, different simulation methods.
+type Ticker interface {
+	// Get the current value of a ticker.
+	//
+	// Note that no ticker value may be below zero, as a rule of business logic.
+	Value() float64
+
+	// Update the value of a ticker.
+	//
+	// All updates should finish by ensuring the ticker value is non-negative,
+	// that is, clamp the updated value to be zero or larger.
+	Update()
+}
