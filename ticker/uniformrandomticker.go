0d43d61c5b4d320c6ec4750e2b42ef89e76ada1e
Hayden McAlister <haydenmcalister49@gmail.com>
2025-09-06 21:08:23 +1200

    Add thread safe syncronization to the uniform random ticker

--- ticker/uniformrandomticker.go
+++ ticker/uniformrandomticker.go
@@ -1,19 +1,26 @@
 package ticker
 
-import "math/rand"
+import (
+	"math/rand"
+	"sync"
+)
 
 type UniformRandomTicker struct {
 	name        string
 	Value       float64 `mapstructure:"Value"`
 	RandomRange float64 `mapstructure:"RandomRange"`
 	randGen     *rand.Rand
+	mu          sync.Mutex
 }
 
-func (t UniformRandomTicker) String() string {
+func (t *UniformRandomTicker) String() string {
 	return t.name
 }
 
-func (t UniformRandomTicker) Validate() error {
+func (t *UniformRandomTicker) Validate() error {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+
 	if t.Value < 0.0 {
 		return ErrorNegativeValueTicker
 	}
@@ -25,11 +32,17 @@ func (t UniformRandomTicker) Validate() error {
 	return nil
 }
 
-func (t UniformRandomTicker) GetValue() float64 {
+func (t *UniformRandomTicker) GetValue() float64 {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+
 	return t.Value
 }
 
 func (t *UniformRandomTicker) Update() {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+
 	t.Value += -t.RandomRange + 2*t.RandomRange*t.randGen.Float64()
 	if t.Value < 0 {
 		t.Value = 0
