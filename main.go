4753e837529698ac173190dd30683d9bb935b5b5
Hayden McAlister <haydenmcalister49@gmail.com>
2025-09-07 17:07:58 +1200

    Complete shift of ticker update logic to ticker package

--- main.go
+++ main.go
@@ -25,45 +25,13 @@ func main() {
 	tickers := ticker.ParseTickers()
 	slog.Debug("parsed tickers", "tickers", tickers)
 
+	// --------------------------------------------------------------------------------
+
 	updatePeriod := time.Duration(viper.GetInt64("UpdatePeriod")) * time.Nanosecond
 	var tickerWaitGroup sync.WaitGroup
-	for _, ticker := range tickers {
+	for _, t := range tickers {
 		tickerWaitGroup.Go(func() {
-			// An unfortunate name, time.Ticker is a timing device to count a certain time before updating.
-			// We will refer to this as the timer throughout to avoid confusion with a stock ticker.
-			timer := time.NewTicker(updatePeriod)
-
-			for updateTimerTimestamp := range timer.C {
-				updateStartTime := time.Now()
-				ticker.Update()
-				updateDuration := time.Since(updateStartTime)
-
-				slog.Debug("ticker updated",
-					slog.Group("ticker",
-						"name", ticker.String(),
-						"value", ticker.GetValue(),
-					),
-					slog.Group("timing",
-						"timestamp", updateTimerTimestamp,
-						"updateDuration", updateDuration,
-						"expectedUpdatePeriod", updatePeriod,
-					),
-				)
-
-				if updateDuration > updatePeriod {
-					slog.Warn("timer update is lagging behind update period",
-						slog.Group("ticker",
-							"name", ticker.String(),
-							"value", ticker.GetValue(),
-						),
-						slog.Group("timing",
-							"timestamp", updateTimerTimestamp,
-							"updateDuration", updateDuration,
-							"expectedUpdatePeriod", updatePeriod,
-						),
-					)
-				}
-			}
+			ticker.StartTicker(t, updatePeriod)
 		})
 	}
 	tickerWaitGroup.Wait()
