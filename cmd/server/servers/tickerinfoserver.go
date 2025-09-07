a856fb00d06307fc7b030a02925b9ffef399b732
Hayden McAlister <haydenmcalister49@gmail.com>
2025-09-07 20:43:57 +1200

    Implement the GetAllTickerName methods

--- cmd/server/servers/tickerinfoserver.go
+++ cmd/server/servers/tickerinfoserver.go
@@ -8,22 +8,48 @@ import (
 	"connectrpc.com/connect"
 	"github.com/hmcalister/genron/cmd/server/ticker"
 	tickerv1 "github.com/hmcalister/genron/gen/api/ticker/v1"
+	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 var (
 	ErrorTickerDoesNotExist = connect.NewError(connect.CodeNotFound, errors.New("no ticker exists with requested name"))
 )
 
-type GetTickerValueServer struct {
+type TickerInfoServer struct {
 	// A map from ticker name to ticker structs
-	Tickers map[string]ticker.Ticker
+	Tickers     map[string]ticker.Ticker
+	TickerNames []string
 }
 
-func (serv *GetTickerValueServer) GetTickerValue(
+func (serv *TickerInfoServer) GetAllTickerNames(
+	ctx context.Context,
+	req *connect.Request[emptypb.Empty],
+) (*connect.Response[tickerv1.GetAllTickerNamesResponse], error) {
+	slog.Info("new get all ticker names request",
+		"ctx", ctx,
+		"reqHeader", req.Header(),
+		"reqMsg", req.Msg,
+	)
+
+	if err := ctx.Err(); err != nil {
+		return nil, err
+	}
+
+	res := connect.NewResponse(&tickerv1.GetAllTickerNamesResponse{
+		TickerName: serv.TickerNames,
+	})
+	return res, nil
+}
+
+func (serv *TickerInfoServer) GetTickerValue(
 	ctx context.Context,
 	req *connect.Request[tickerv1.GetTickerValueRequest],
 ) (*connect.Response[tickerv1.GetTickerValueResponse], error) {
-	slog.Info("new get ticker value request", "ctx", ctx, "reqHeader", req.Header(), "reqMsg", req.Msg)
+	slog.Info("new get ticker value request",
+		"ctx", ctx,
+		"reqHeader", req.Header(),
+		"reqMsg", req.Msg,
+	)
 
 	if err := ctx.Err(); err != nil {
 		return nil, err
